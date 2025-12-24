4ac9655155f8f69c4c91728029f0cf0a8bf03862
apple <105837597+380583067@users.noreply.github.com>
2025-12-24 20:09:54 +0800

    for S

---
+++ patches/uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rootdirectory="$PWD"
+dirs="bionic/libm external/selinux frameworks/native frameworks/av hardware/interfaces system/extras system/core system/bpf system/libhwbinder" 
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Cleaning $dir patches..."
+	git checkout -- . && git clean -df
+done
+
+echo "Done!"
+cd $rootdirectory
